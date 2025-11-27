cea5557a02aa20ebd18b90e81706b413abd5a6d4
pranavkrishna-sn <pranav05krish@gmail.com>
2025-11-27 11:59:10 +0530

    Created file via n8n

---
+++ backend/setup-core-infrastructure/models/models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class SetupCoreInfrastructure(Base):
+    __tablename__ = 'setup_core_infrastructure'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
