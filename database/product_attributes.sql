c47f93887fb005a4a0bc4b45ffb667c34a25fce5
pranavkrishna05 <pranav2002krishna@gmail.com>
2026-01-06 21:57:50 +0530

    Delete database directory

--- database/product_attributes.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE product_attributes (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    product_id INTEGER NOT NULL,
-    attribute_name TEXT NOT NULL,
-    attribute_value TEXT NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE ON UPDATE CASCADE
-);
