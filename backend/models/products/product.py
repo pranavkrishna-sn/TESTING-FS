0f81e0a9b3ec05122eebb7490bbd1326710c510c
pranavkrishna05 <pranav2002krishna@gmail.com>
2026-01-07 15:46:44 +0530

    commited by n8n

---
+++ backend/models/products/product.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from backend import db
+
+class Product(db.Model):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __init__(self, name: str, price: float, description: str) -> None:
+        self.name = name
+        self.price = price
+        self.description = description
