72d4fc5d2287f86679c255ccf095739a59451a82
pranavkrishna05 <pranav2002krishna@gmail.com>
2026-01-07 15:46:05 +0530

    commited by n8n

---
+++ backend/repositories/users/user_repository.py
@@ -0,0 +1,11 @@
+from backend.models.users.user import User
+
+class UserRepository:
+    @staticmethod
+    def find_by_email(email: str) -> User:
+        return User.query.filter_by(email=email).first()
+
+    @staticmethod
+    def save(user: User) -> None:
+        db.session.add(user)
+        db.session.commit()
